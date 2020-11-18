e76e0dd33b2160ca6867a3a0b8a2ce21f1cc2732
AFTM7512 <jmethods@163.com>
2020-11-18 22:19:58 +0800

    add 增加读取目录下文件的方法

--- static-list/index.js
+++ static-list/index.js
@@ -1,19 +1,40 @@
-const fs   = require('fs')
+const fs = require('fs')
 const path = require('path')
 
-const { resolve } = path
-const { readdir, stat } = fs
+const { resolve, extname } = path
+const { readdir, readdirSync, stat } = fs
 
 /**
  * 检索出当前页面的文件列表名称。
  * @param  { String }   path     : 查询的路径，必须； 
- * @param  { String }   extname  : 
- * @param  { Function } callback :
+ * @param  { String | Object }   options  : 如果 options 是个字符串，则它表示 extname
+ *          extname:    过滤出需要的后缀文件；
+ *          recursion:  递归出所有文件
  * @return { Array } 返回一个当前路径对应的文件列表。
  */
-function getDirList(path, extname, callback) {
-  console.log(path);
-  if (!path) new Error('path is require')
+function getDirList(path, options) {
+  if (!path) throw new Error('path is require')
+
+  // let result = []
+  // readdir(path, (error, files) => {
+  //   if (error) throw error
+  //   // 判断 options 
+  //   if (typeof options === 'string') {
+  //     result = files.filter(item => extname(item) && extname(item).substring(1) === options)
+  //   } else if (typeof options === 'object') {
+
+  //   } else {
+  //     result = files
+  //   }
+  // })
+  // console.log(result);
+
+  const files = readdirSync(path)
+  if (files && typeof options === 'string') {
+    return files.filter(item => extname(item) && extname(item).substring(1) === options)
+  } else {
+    return files
+  }
 }
 
 module.exports = getDirList
--- static-list/test.js
+++ static-list/test.js
@@ -1,3 +1,5 @@
+const path = require('path')
 const getDir = require('./index.js')
 
-console.log(getDir);
+const result = getDir(path.resolve(__dirname, './assets'), 'jpg')
+console.log(result);
